008086a1540b84001d9740ff7353b15622d8aa4f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-05-27 09:21:32 +0100

    added authorship and date

--- JavaSololearnProject/Loan_Calculator/LoadCalculator.java
+++ JavaSololearnProject/Loan_Calculator/LoadCalculator.java
@@ -2,6 +2,8 @@ package JavaSololearnProject.Loan_Calculator;
 
 import java.util.Scanner;
 
+//Author: KaptainCS3
+//Date Fri 27 May 2022
 public class LoadCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
