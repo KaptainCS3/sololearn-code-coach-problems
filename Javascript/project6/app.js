7341052a1a27ef9e01f7d8aa29ed5a2996e1391c
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-04 11:53:22 +0100

    print method unfinished

--- Javascript/project6/app.js
+++ Javascript/project6/app.js
@@ -1,12 +1,26 @@
-class Add{
-    constructor(...words){
-        this.words = words
-    }
+class Add {
+  constructor(...words) {
+    this.words = words;
+    const mapData = words.map((el) => {
+      return el[0].replace("", "$") + el.slice(1);
+    });
+    const data = mapData.toString();
+    console.log(data.match(/,/g));
+  }
 }
 
-var x = new Add("hehe", "hoho", "haha", "hihi", "huhu")
-var y = new Add("this", "is", "awesome")
-var z = new Add("lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit")
-x.print()
-y.print()
-z.print()
+var x = new Add("hehe", "hoho", "haha", "hihi", "huhu");
+var y = new Add("this", "is", "awesome");
+var z = new Add(
+  "lorem",
+  "ipsum",
+  "dolor",
+  "sit",
+  "amet",
+  "consectetur",
+  "adipiscing",
+  "elit"
+);
+x.print();
+y.print();
+z.print();
