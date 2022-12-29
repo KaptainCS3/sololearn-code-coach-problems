48a5e36f2d403fb94f5d254b8e6d0b7d3dfc8b20
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-29 23:25:26 +0100

    Update Argentina.cpp

--- Easy-exercises/Argentina/Argentina.cpp
+++ Easy-exercises/Argentina/Argentina.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namepace std;
+#define CENT_RATE 2
+int main(){
+    int pesos, dollar, rate;
+    cout <<"enter value for Pesos: " << endl;
+    cin >> pesos;
+    cout << "enter value for Dollar: " << endl;
+    cin >> dollar;
+   //convert dollar to pesos
+    rate = (50 * dollar);
+    rate > pesos ? cout << "Pesos";
+                 : cout << "Dollars";
+    return 0;
+}
