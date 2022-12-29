9ad6afe29768cd4547789d2da1af1a25f83b16a5
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-29 23:31:24 +0100

    Update Cheer_Generator.cpp

--- Easy-exercises/Cheer_Generator/Cheer_Generator.cpp
+++ Easy-exercises/Cheer_Generator/Cheer_Generator.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namepace std;
+int main(){
+    int yard, i = 1;
+    cout <<"enter value for yard : " << endl;
+    cin >> yard;
+    (yard > 10) ? cout << "High Five";
+              : (yard < 1) ? cout << "shh"
+                           : cout << "";
+        while(i <= yard){
+            printf("Ra!");
+            i++;
+        }
+    }
+    return 0;
+}
