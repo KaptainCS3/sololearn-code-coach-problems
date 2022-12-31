2e78a64250ee2d3663a00746e67f576c12d7925d
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-12-31 13:21:46 +0100

    Update Popsicle.cpp

--- Easy-exercises/Popsicles/Popsicle.cpp
+++ Easy-exercises/Popsicles/Popsicle.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+
+// done by Leonard Appelgryn(KAPTAINCS3)
+using namespace std;
+int main(void){
+
+    int siblings, popsicles;
+
+    cout<<"Enter number of siblings: " << endl;
+
+cin >> siblings;
+
+cout <<"Enter number of Popsicles: " << endl;
+
+cin >> popsicles;
+
+    (popsicles % siblings != 0) ?
+
+    cout <<"eat them yourself" << endl
+    :
+
+
+    printf("give away\n");
+
+}
