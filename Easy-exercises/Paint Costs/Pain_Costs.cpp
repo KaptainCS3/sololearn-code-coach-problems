0a84e9c048166f478ee2bef1a8421fa114a99e40
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2023-01-01 20:25:25 +0100

    Update Pain_Costs.cpp

--- Easy-exercises/Paint Costs/Pain_Costs.cpp
+++ Easy-exercises/Paint Costs/Pain_Costs.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+#include<cmath>
+#define canvas_brushes 40.00
+using namespace std;
+int main(void){
+    float paint, res, tax, total;
+    cout <<"Enter number of colors: " << endl;
+    cin >>paint;
+    res = ((paint * 5.00) + canvas_brushes);
+    tax = ceil((res * 10) / 100);
+    total = ceil(tax + res);
+    cout << fixed;
+    cout.precision(1);
+    cout <<"Total cost for project is : ", << total;
+}
