f05c7d08dc619cad4ce8daf7a05cc8f8b0be39b9
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2023-01-01 20:30:57 +0100

    Update Halloween_Candy.cpp

--- Easy-exercises/Halloween Candy/Halloween_Candy.cpp
+++ Easy-exercises/Halloween Candy/Halloween_Candy.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<cmath>
+// done by Leonard Appelgryn(KAPTAINCS3)
+int main(){
+    int houses, out_put;
+    float res = 2 * 100;
+    cout <<"enter number of houses visited" <<endl;
+    cin >>houses;
+    out_put = ceil(res/houses);
+    cout <<"percentage is " <<out_put<<"%" <<endl;
+    return 0;
+}
