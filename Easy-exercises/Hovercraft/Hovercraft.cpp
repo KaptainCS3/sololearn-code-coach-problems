b03af0d99901bfdfbe25614fda6ab4496cad8446
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-03 14:57:55 +0100

    hovercraft in c++

--- Easy-exercises/Hovercraft/Hovercraft.cpp
+++ Easy-exercises/Hovercraft/Hovercraft.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+using namespace std;
+// done by Leonard Appelgryn(KAPTAINCS3)
+#define amt_in 21000000
+int main(void)
+{
+    int customer, new_amt;
+    cout <<"Enter number of Customers: " << endl;
+    scanf("%d", &customer);
+    new_amt = 3000000 * customer;
+    (new_amt > amt_in) ? cout <<"Profit " << endl
+                       : (new_amt == amt_in) ? cout <<"Broke Even " << endl
+                       :  cout <<"Loss " << endl;
+}
