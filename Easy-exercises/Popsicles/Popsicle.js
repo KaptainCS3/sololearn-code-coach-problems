7f1ee74e9f52afebc6a113c3b386c7ee2c0b2a33
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-03 14:42:42 +0100

    popsicle function done

--- Easy-exercises/Popsicles/Popsicle.js
+++ Easy-exercises/Popsicles/Popsicle.js
@@ -1,1 +1,4 @@
-
+ const Popsicle = (siblings, popsicle)=>{
+    (popsicle % siblings != 0) ? console.log("eat them yourself") : console.log("give away");
+ }
+ Popsicle(parseInt(prompt("Enter number of siblings: ")), parseInt(prompt("Enter number of Popsicles: ")))
