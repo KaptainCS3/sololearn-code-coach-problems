18c8cf3373e2e7737cc374624018fe1fd08c94ae
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-03 14:11:42 +0100

    resolve nested tenary opeartors

--- Easy-exercises/Gotham City/Gotham_City.cpp
+++ Easy-exercises/Gotham City/Gotham_City.cpp
@@ -4,8 +4,8 @@ int main(){
 int criminals;
     cout <<"Enter number of criminals: " << endl;
     cin >> criminals;
-    criminals > 10 ? court <<"Good Luck out there! " << endl; 
-    : crimals >= 5 && criminals < 10 ? cout <<"Help me Batman " <<endl; 
+    criminals > 10 ? cout <<"Good Luck out there! " << endl
+    : (criminals >= 5 && criminals < 10) ? cout <<"Help me Batman " << endl 
     : cout <<"I got this! " <<endl;
     return 0;
 }
