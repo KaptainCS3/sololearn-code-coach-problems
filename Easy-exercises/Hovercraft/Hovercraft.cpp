aa1bac213df89f63c3488d229e9c59a7965e44c7
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-03 15:07:33 +0100

    code format

--- Easy-exercises/Hovercraft/Hovercraft.cpp
+++ Easy-exercises/Hovercraft/Hovercraft.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main(void)
 {
     int customer, new_amt;
-    cout <<"Enter number of Customers: " << endl;
+    cout << "Enter number of Customers: " << endl;
     scanf("%d", &customer);
     new_amt = 3000000 * customer;
-    (new_amt > amt_in) ? cout <<"Profit " << endl
-                       : (new_amt == amt_in) ? cout <<"Broke Even " << endl
-                       :  cout <<"Loss " << endl;
+    (new_amt > amt_in)    ? cout << "Profit " << endl
+    : (new_amt == amt_in) ? cout << "Broke Even " << endl
+                          : cout << "Loss " << endl;
 }
