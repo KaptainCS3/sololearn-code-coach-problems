37e34aba4ea4bf1e9056fc331e505832ae8924aa
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-12 23:17:26 +0100

    destructure array

--- Easy-exercises/Jungle Camping/Jungle _Camping.js
+++ Easy-exercises/Jungle Camping/Jungle _Camping.js
@@ -1,1 +1,7 @@
-
+const jungleCamping = () => {
+  const animals = ["Lions", "Tigers", "Snakes", "Birds"];
+  
+  const [Grr, Rawr, Ssss, Chirp] = animals;
+//   console.log(Grr);
+};
+jungleCamping()
