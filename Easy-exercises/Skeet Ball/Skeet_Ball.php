4f4a0c0e88b91b635229243ad355c795518fe6ce
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-12 14:37:33 +0100

    initial commit

---
+++ Easy-exercises/Skeet Ball/Skeet_Ball.php
@@ -0,0 +1,10 @@
+<?php
+    echo ("Enter number of points\n");
+    $points =(int)readline("");
+    echo ("Enter number of tickets\n");
+    $tickets =(int)readline("");
+    if($rate > $point)
+    echo ("Try again\n");
+    else
+    echo ("Buy it!\n");
+?>
