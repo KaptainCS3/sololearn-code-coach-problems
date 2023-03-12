2906707fd135b5266e56f3a434086f2380cfc88e
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-12 14:34:58 +0100

    initial commit

---
+++ Easy-exercises/Halloween Candy/Halloween_Candy.php
@@ -0,0 +1,9 @@
+<?php
+// done by Leonard Appelgryn(KAPTAINCS3)
+    $res = 2 * 100;
+    echo ("enter number of houses visited\n");
+    $houses = (int)readline("");
+    $out_put = ceil($res/$houses);
+    echo "percentage is $out_put";
+
+?>
